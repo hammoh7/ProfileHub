25265b77d176fe87ea7246487e6d0c4b935b4691
hammoh7 <soahammohaadkar@gmail.com>
2024-04-27 23:31:35 +0530

    App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from '../components/login/login.component';
+import { RegisterComponent } from '../components/register/register.component';
+import { ProfileComponent } from '../components/profile/profile.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +16,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'profilehub';
+  title = 'ProfileHub';
 }
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from '../components/register/register.component';
+import { LoginComponent } from '../components/login/login.component';
+import { ProfileComponent } from '../components/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+];
