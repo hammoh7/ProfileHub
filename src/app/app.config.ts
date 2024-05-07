2b946452aa64175fdca5c07e6f92fe50de8afd6b
hammoh7 <soahammohaadkar@gmail.com>
2024-05-07 23:40:55 +0530

    App

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,3 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
-
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000' // Update this URL to match your Node.js server
-};
