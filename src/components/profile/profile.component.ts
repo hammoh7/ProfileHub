22690ecc7a23d1993c8a833ce1639a2391a7e760
hammoh7 <soahammohaadkar@gmail.com>
2024-05-07 23:41:51 +0530

    Profile components

--- src/components/profile/profile.component.ts
+++ src/components/profile/profile.component.ts
@@ -1,24 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../app/app.config';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-  // username: string = 'Jack Reacher'; 
-  user: any;
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.fetchUserProfile();
-  }
-
-  fetchUserProfile() {
-    this.http.get(`${environment.apiUrl}/profile`).subscribe((data: any) => {
-      this.user = data;
-    });
-  }
+export class ProfileComponent {
+  username: string = '';
 }
